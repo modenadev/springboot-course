8fe6aa51338fa3b4fde74221e46603a01456d472
Ruan <ruanm0dena@outlook.com>
2024-09-04 15:01:10 -0300

    h2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,18 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
@@ -15,6 +22,10 @@ public class User implements Serializable {
 
 
     public User(long l, String maria, String mail, String number, String ruan1234) {
+    }
+
+    public User() {
+
     }
 
     public long getId() {
